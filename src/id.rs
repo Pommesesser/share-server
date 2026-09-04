ed33284a917adc117fa8f0f16286d63a67fe5bb1
david <david.lu.singh@gmail.com>
2026-09-04 08:15:35 +0200

    id is now 8 bytes only

--- src/id.rs
+++ src/id.rs
@@ -1,7 +1,7 @@
 use rand::RngExt;
 use base64::{engine::general_purpose::URL_SAFE_NO_PAD, Engine as _};
 
-const N: usize = 16;
+const N: usize = 8;
 
 pub fn gen_rand_id() -> String {
     let mut bytes = [0u8; N];
